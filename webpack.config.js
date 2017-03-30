ea0f91e4e86b25142d2bbb8eb17be553ec383a63
olhasheremeta <osheremeta91@gmail.com>
2017-03-30 21:21:27 +0200

    Starting project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: './index.js',
+
+  output: {
+    path: path.join(__dirname + '/dist'),
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve('index.html'),
+        inject: 'body'
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+
+    new webpack.NoErrorsPlugin(),
+  ],
+
+  devtool: 'source-map',
+};
