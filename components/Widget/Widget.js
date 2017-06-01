5a1f9fd89433755395547c5a09edbad050b89294
olhasheremeta <osheremeta91@gmail.com>
2017-06-01 22:15:03 +0200

    creating title element

--- components/Widget/Widget.js
+++ components/Widget/Widget.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import Button from '../Button/Button';
 
 import './Widget.scss'
 
 class Widget extends React.Component {
+  state = {
+    isRunning: false
+  }
+
+  start = () => {
+    this.setState({
+      isRunning: true
+    });
+  }
+
   render() {
     return (
       <div className="Widget">
-        { this.props.children }
+        <div className="Widget-title">{this.props.title}</div>
+        <div className="Widget-body">
+          {
+            this.state.isRunning
+             ? this.props.children
+             : (
+              <div className="Widget-body-overlay">
+                <Button OnClick={this.start} className="Start-btn">Start</Button>
+              </div>
+             )
+          }          
+          </div>
       </div>
     );
   }
